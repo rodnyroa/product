dbd00ffff9ff12f24b42b9c5d1bf1ed1e54f7588
Rodny Roa <rodny.roa@analytika.es>
2022-01-27 19:43:18 +0100

    find customer by id

--- src/main/java/com/example/product/core/customer/service/impl/CustomerSeekerImpl.java
+++ src/main/java/com/example/product/core/customer/service/impl/CustomerSeekerImpl.java
@@ -3,6 +3,7 @@ package com.example.product.core.customer.service.impl;
 import com.example.product.core.customer.domain.Customer;
 import com.example.product.core.customer.repository.CustomerRepository;
 import com.example.product.core.customer.service.CustomerSeeker;
+import com.example.product.core.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,5 +22,12 @@ public class CustomerSeekerImpl implements CustomerSeeker {
     @Override
     public Page<Customer> findAll(Pageable pageable) {
         return this.repository.findAll(pageable);
+    }
+
+    @Override
+    public Customer findById(Integer customerId) {
+        String msg = String.format("Invalid id: %s", customerId);
+        return this.repository.findById(customerId)
+                .orElseThrow(() -> new NotFoundException(msg));
     }
 }
--- src/test/java/com/example/product/unit/customer/CustomerSeekerTests.java
+++ src/test/java/com/example/product/unit/customer/CustomerSeekerTests.java
@@ -4,7 +4,7 @@ import com.example.product.core.customer.domain.Customer;
 import com.example.product.core.customer.repository.CustomerRepository;
 import com.example.product.core.customer.service.CustomerSeeker;
 import com.example.product.core.customer.service.impl.CustomerSeekerImpl;
-import com.example.product.core.product.domain.Product;
+import com.example.product.core.exception.NotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -19,6 +19,7 @@ import org.springframework.data.domain.PageRequest;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Optional;
 
 @DisplayName("Product Seeker")
 @ExtendWith(MockitoExtension.class)
@@ -57,6 +58,25 @@ public class CustomerSeekerTests extends CustomerUtil {
 
         Assertions.assertNotNull(page);
         Assertions.assertFalse(page.hasContent());
+    }
+
+    @DisplayName("findById returns value")
+    @Test
+    void findById() {
+        Customer mock = this.getCustomerMock();
+        Mockito.when(this.repository.findById(Mockito.anyInt()))
+                .thenReturn(Optional.of(mock));
+        Customer customer = this.seeker.findById(ID);
+        Assertions.assertNotNull(customer);
+
+    }
+
+    @DisplayName("when findById with invalid id should throw NotFoundException")
+    @Test
+    void findByIdShouldThrowException() {
+        Mockito.when(this.repository.findById(Mockito.anyInt()))
+                .thenReturn(Optional.empty());
+        Assertions.assertThrows(NotFoundException.class, () -> this.seeker.findById(ID));
     }
 
 }
