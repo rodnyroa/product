7f2245340cfaec17ad5cb3e270aac4fe59acec15
Rodny Roa <rodny.roa@analytika.es>
2022-01-30 18:04:28 +0100

    db postgresql

---
+++ src/main/resources/schema-postgresql.sql
@@ -0,0 +1,49 @@
+--DROP SCHEMA IF EXISTS public;
+
+CREATE SCHEMA IF NOT EXISTS public;
+
+
+CREATE SEQUENCE IF NOT EXISTS public.product_dim_seq 	INCREMENT 1 	MINVALUE 1 	MAXVALUE 9223372036854775807 	START 1 	CACHE 1;
+
+create table public.product_dim
+(
+	id integer not null DEFAULT nextval('product_dim_seq'),
+	name varchar(100) not null,
+	price numeric not null,
+	constraint product_dim_pkey primary key(id)
+)
+;
+
+
+
+CREATE SEQUENCE IF NOT EXISTS public.customer_dim_seq 	INCREMENT 1 	MINVALUE 1 	MAXVALUE 9223372036854775807 	START 1 	CACHE 1;
+
+create table public.customer_dim
+(
+	id integer not null DEFAULT nextval('customer_dim_seq'),
+	firstname varchar(100) not null,
+	lastname varchar(100) not null,
+	email varchar(100) not null,
+	constraint customer_dim_pkey primary key(id)
+)
+;
+
+create table public.order_fact
+(
+	customer_id bigint not null
+		constraint fk_order_fact_01
+			references customer_dim,
+	product_id bigint not null
+		constraint fk_order_fact_02
+			references product_dim,
+	order_time date not null,
+	order_number varchar(50) not null,
+	order_line_number integer not null,
+	order_line_quantity smallint not null,
+	total_price_line numeric not null
+)
+;
+
+
+--alter sequence product_dim_seq restart with 101;
+--alter sequence customer_dim_seq restart with 11;
